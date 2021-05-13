64a50de34ee96bdc960764eaeef4ac84b69236e6
Ivanef-StrgV <59922570+Ivanef-StrgV@users.noreply.github.com>
2021-05-13 19:14:52 +0200

    Updated the logger

--- Logger.cs
+++ Logger.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/****************************************************************
+ *   ______                _                                    *
+ *  |  ____|              | |                                   *
+ *  | |__   __ _ ___ _   _| |     ___   __ _  __ _  ___ _ __    *
+ *  |  __| / _` / __| | | | |    / _ \ / _` |/ _` |/ _ \ '__|   *
+ *  | |___| (_| \__ \ |_| | |___| (_) | (_| | (_| |  __/ |      *
+ *  |______\__,_|___/\__, |______\___/ \__, |\__, |\___|_|      *
+ *                    __/ |             __/ | __/ |             *
+ *                   |___/             |___/ |___/              *
+ *  by basicx-StrgV                                             *
+ *  https://github.com/basicx-StrgV/EasyLogger                  *
+ *                                                              *
+ * **************************************************************/
+using System;
 using System.IO;
 using EasyLogger.LoggerFile;
 using EasyLogger.LoggerDirectory;
@@ -7,120 +20,86 @@ namespace EasyLogger
 {
     public class Logger
     {
-        public LogFile logFile { get; set; }
-        public LogDirectory logDirectory { get; set; }
-        public string dateFormate { get; set; } = "yyyy'/'MM'/'dd HH:mm:ss";
+        public LogFile logFile { get; }
+        public LogDirectory logDirectory { get; }
+        public string dateFormate { get; } = "yyyy'/'MM'/'dd HH:mm:ss";
 
         public Logger()
         {
-            try
-            {
-                this.logFile = new LogFile("log", LogFileType.txt);
-                this.logDirectory = new LogDirectory(Environment.CurrentDirectory, "Logs");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            this.logFile = new LogFile("log", LogFileType.txt);
+            this.logDirectory = new LogDirectory(Environment.CurrentDirectory, "Logs");
+            createDirectory();
         }
 
         public Logger(LogFile logFile)
         {
-            try
-            {
-                this.logFile = logFile;
-                this.logDirectory = new LogDirectory(Environment.CurrentDirectory, "Logs");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            this.logFile = logFile;
+            this.logDirectory = new LogDirectory(Environment.CurrentDirectory, "Logs");
+            createDirectory();
         }
 
         public Logger(LogDirectory logDirectory)
         {
-            try
-            {
-                this.logFile = new LogFile("log", LogFileType.txt);
-                this.logDirectory = logDirectory;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            this.logFile = new LogFile("log", LogFileType.txt);
+            this.logDirectory = logDirectory;
+            createDirectory();
         }
 
         public Logger(LogFile logFile, LogDirectory logDirectory)
         {
-            try
-            {
-                this.logFile = logFile;
-                this.logDirectory = logDirectory;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            this.logFile = logFile;
+            this.logDirectory = logDirectory;
+            createDirectory();
         }
 
         public Logger(string dateFormate)
         {
-            try
-            {
-                this.logFile = new LogFile("log", LogFileType.txt);
-                this.logDirectory = new LogDirectory(Environment.CurrentDirectory, "Logs");
-                this.dateFormate = dateFormate;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            this.logFile = new LogFile("log", LogFileType.txt);
+            this.logDirectory = new LogDirectory(Environment.CurrentDirectory, "Logs");
+            this.dateFormate = dateFormate;
+            createDirectory();
         }
 
         public Logger(LogFile logFile, string dateFormate)
         {
-            try
-            {
-                this.logFile = logFile;
-                this.logDirectory = new LogDirectory(Environment.CurrentDirectory, "Logs");
-                this.dateFormate = dateFormate;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            this.logFile = logFile;
+            this.logDirectory = new LogDirectory(Environment.CurrentDirectory, "Logs");
+            this.dateFormate = dateFormate;
+            createDirectory();
         }
 
         public Logger(LogDirectory logDirectory, string dateFormate)
         {
-            try
-            {
-                this.logFile = new LogFile("log", LogFileType.txt);
-                this.logDirectory = logDirectory;
-                this.dateFormate = dateFormate;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            this.logFile = new LogFile("log", LogFileType.txt);
+            this.logDirectory = logDirectory;
+            this.dateFormate = dateFormate;
+            createDirectory();
         }
 
         public Logger(LogFile logFile, LogDirectory logDirectory, string dateFormate)
         {
-            try
-            {
-                this.logFile = logFile;
-                this.logDirectory = logDirectory;
-                this.dateFormate = dateFormate;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            this.logFile = logFile;
+            this.logDirectory = logDirectory;
+            this.dateFormate = dateFormate;
+            createDirectory();
         }
 
+        /// <summary>
+        /// Writes the given message and the current time stamp to the log file.
+        /// </summary>
+        /// <remarks>
+        /// If the log file and/or directory is missing, the method will automatically create them.
+        /// </remarks>
+        /// <exception cref="System.ArgumentException"></exception>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        /// <exception cref="System.NotSupportedException"></exception>
+        /// <exception cref="System.UnauthorizedAccessException"></exception>
+        /// <exception cref="System.IO.IOException"></exception>
+        /// <exception cref="System.IO.PathTooLongException"></exception>
+        /// <exception cref="System.IO.DirectoryNotFoundException"></exception>
+        /// <exception cref="System.Security.SecurityException"></exception>
         public void log(string message)
-        { 
+        {
             try
             {
                 if (!Directory.Exists(logDirectory.ToString()))
@@ -135,7 +114,7 @@ namespace EasyLogger
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                throw e;
             }
         }
 
@@ -146,10 +125,9 @@ namespace EasyLogger
                 DateTime systemTime = DateTime.Now;
                 return systemTime.ToString(dateFormate);
             }
-            catch(Exception e)
+            catch(Exception)
             {
-                Console.WriteLine(e);
-                return "ERROR 0000/00/00 00:00:00";
+                return "INVALID DATE FORMAT";
             }
         }
 
@@ -164,7 +142,7 @@ namespace EasyLogger
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                throw e;
             }
         }
     }
--- README.md
+++ README.md
@@ -2,11 +2,19 @@
 An easy to use logger for any C# project.
 
 You can just use the log method to create and write a default log file
-or customize the settings of the logger to create a custom log file.
+or create the logger object with custo settings to create a custom log file.
  
 ## How to use
 
 You can clone the repository and add EasyLogger to your existing solution in VisualStudio
+
+### Namespaces
+
+- EasyLogger
+- EasyLogger.LoggerFile
+- EasyLogger.LoggerDirectory
+
+### Sample
 
 ```cs
 using System;
